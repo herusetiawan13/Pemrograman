394f0efa8a7578016731cd67c0f39417130b5c2d
herusetiawan13 <herusetiawan1304@gmail.com>
2018-11-11 02:41:52 +0700

    Revert "Revert "Revert "Revert "Update Algoritma Pemrograman/Halaman 41/TugasNo3While.cpp""""
    This reverts commit bc78b5ec41dcc568e6deb9678a8374706a59cf62.

---
+++ Algoritma Pemrograman/Halaman 41/TugasNo3While.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <conio.h>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int a, n;
+	float nilai, rata;
+	nilai=0;
+	cout<<"Masukkan Nilai = "; cin>>n;
+	
+	while (a<=n){
+		nilai=nilai+a;
+		a++;
+	}
+	cout<<"Nilai Total = "<<nilai<<endl;
+	rata=nilai/n;
+	cout<<"Rata-rata = "<<rata<<endl;
+return 0;
+}
