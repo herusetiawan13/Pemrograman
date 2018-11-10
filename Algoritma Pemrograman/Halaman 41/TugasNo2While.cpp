7e84cfe835cd66deed1fdb251f3179b9597af519
herusetiawan13 <herusetiawan1304@gmail.com>
2018-11-11 02:37:05 +0700

    Update Algoritma Pemrograman/Halaman 41/TugasNo2While.cpp
    Tugas Pertemuan 7
    
    Co-Authored-By: Antonius Heru Setiawan <herusetiawan@users.noreply.github.com>

---
+++ Algoritma Pemrograman/Halaman 41/TugasNo2While.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int a, n;
+	float nilai=0;
+	cout<<"Masukkan Bilangan = "; cin>>n;
+	while (a<=n){
+		nilai=nilai+a;
+		a++;
+	}
+	cout<<"Total Bilangan = "<<nilai<<endl;
+return 0;
+}
